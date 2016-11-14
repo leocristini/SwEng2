bd8b0ad4c7efc5d55aab402f75fd994fc21d11d4
leocristini <leonardocristini@gmail.com>
2016-11-14 12:07:19 +0100

    aggiunta funzionalità di aggiungere colonne

--- javascript.js
+++ javascript.js
@@ -6,11 +6,19 @@ function showForm(){
 };
 
 function setOrder(){
+            
+    var tmp_it = document.getElementById("it_list").insertCell(0);
+    tmp_it.innerHTML = document.getElementById("it_name").value;
+    var tmp_qty = document.getElementById("qty_list").insertCell(0);
+    tmp_qty.innerHTML = document.getElementById("it_qty").value;    
     
-    var tabela = document.getElementById("main_table");
-    var qty = document.getElementById("it_qty");
-    var name = document.getElementById("it_name");
-    
-    tabela.insertRow(1);
-    
-}
+    var form = document.getElementById("add_form");
+    // collect the form data while iterating over the inputs
+    var data = {};
+    for (var i = 0, ii = form.length; i < ii; ++i) {
+        var input = form[i];
+        if (input.name) {
+            data[input.name] = input.value;
+        }
+    }
+};
