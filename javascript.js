bf5828188acc5bc3966999f2505317a64ada2250
leocristini <leonardocristini@gmail.com>
2016-11-14 16:21:00 +0100

    aggiunta funzionalità ordine

--- javascript.js
+++ javascript.js
@@ -1,24 +1,31 @@
 function showForm(){
     
     var form = document.getElementById("add_form");
-    form.style.display="block";
+    if(form.style.display=="block"){
+        form.style.display = "none";
+        form.reset();
+    }else
+        form.style.display="block";
 
 };
 
 function setOrder(){
-            
-    var tmp_it = document.getElementById("it_list").insertCell(0);
-    tmp_it.innerHTML = document.getElementById("it_name").value;
-    var tmp_qty = document.getElementById("qty_list").insertCell(0);
-    tmp_qty.innerHTML = document.getElementById("it_qty").value;    
     
-    var form = document.getElementById("add_form");
-    // collect the form data while iterating over the inputs
-    var data = {};
-    for (var i = 0, ii = form.length; i < ii; ++i) {
-        var input = form[i];
-        if (input.name) {
-            data[input.name] = input.value;
+    namelist = document.getElementById("main_table").rows[0].cells;
+    qtàlist = document.getElementById("main_table").rows[1].cells;
+    var count = 0;
+    for(var i=0; i < namelist.length; i++){
+        if(namelist[i].innerHTML == document.getElementById("it_name").value){
+            qtàlist[i].innerHTML += document.getElementById("it_qty").value;
+            alert("name ="+namelist[i]+" qtà= "+qtàlist[i]);
+            count++;
         }
     }
+    if(count == 0){
+        var tmp_it = document.getElementById("it_list").insertCell(0);
+        tmp_it.innerHTML = document.getElementById("it_name").value;
+        var tmp_qty = document.getElementById("qty_list").insertCell(0);
+        tmp_qty.innerHTML = document.getElementById("it_qty").value;    
+    }
+    
 };
