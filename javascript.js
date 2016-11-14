907046822dfd48c13af09b37e260de17271a57a2
leocristini <leonardocristini@gmail.com>
2016-11-14 17:21:17 +0100

    fixed amount

--- javascript.js
+++ javascript.js
@@ -11,6 +11,7 @@ function showForm(){
 
 function setOrder(){
     
+    var maxAmount = document.getElementById("maxAmount").value;
     namelist = document.getElementById("main_table").rows[0].cells;
     qtàlist = document.getElementById("main_table").rows[1].cells;
     var count = 0;
@@ -20,13 +21,31 @@ function setOrder(){
             numqtà += +document.getElementById("it_qty").value;
             qtàlist[i].innerHTML = numqtà;
             count++;
+            if(numqtà > maxAmount){
+                alert("too many items ordered");
+                qtàlist[i].innerHTML = maxAmount;
+            }
         }
     }
     if(count == 0){
         var tmp_it = document.getElementById("it_list").insertCell(0);
         tmp_it.innerHTML = document.getElementById("it_name").value;
         var tmp_qty = document.getElementById("qty_list").insertCell(0);
-        tmp_qty.innerHTML = document.getElementById("it_qty").value;    
+        if(Number(document.getElementById("it_qty").value)<maxAmount){
+            tmp_qty.innerHTML = document.getElementById("it_qty").value;  
+             alert("too many items ordered");
+        }else
+            tmp_qty.innerHTML = maxAmount;
     }
+    
+    
+    
+    
+};
+
+function changeMaxAm(){
+    
+    var maxAmount = document.getElementById("maxAmount").value;
+    document.getElementById("maxAmount") = maxAmount;
     
 };
